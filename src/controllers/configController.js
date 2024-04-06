5447e77b568d37b9fd8bc7c558d6c8af0453a65c
Nauman Khan Gori <nauman@whozzat.com>
2024-04-06 13:46:35 +0530

    public history api

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -14,4 +14,5 @@ router.post('/createbridges',middleware,common.createBridges);  //Done
 router.post('/updatebridges/:bridge_id',middleware,common.updateBridges);  //Done
 router.put('/createbridges/:bridge_id', middleware, common.updateBridges);
 router.delete('/deletebridges/:bridge_id',middleware,common.deleteBridges);
+router.get('/gethistory/:thread_id/:bridge_id',middleware,common.getThreads);  //Public API for getting history for particular thread
 module.exports = router;
