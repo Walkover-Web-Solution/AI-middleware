2feefe481732bf51441126aa9075f6475d7680f6
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-04-17 18:46:59 +0530

    embed token in get_route

--- src/services/commonService/generateToken.js
+++ src/services/commonService/generateToken.js
@@ -1,22 +1,18 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = async (req, res) => {
+function generateToken(bridge_id) {
   try {
-    const { bridge_id } = req.params;
-
     const payload = {
       org_id: process.env.ORG_ID,
       project_id: process.env.PROJECT_ID,
       user_id: bridge_id
     };
     const secretKey = process.env.Access_key;
-    const token = jwt.sign(payload, secretKey);
-    res.status(200).json({ success : true, token });
-
+    return jwt.sign(payload, secretKey);
   } catch (error) {
     console.error("Error generating token:", error);
-    res.status(400).json({success : false, message: 'Failed to generate token' });
+    return null;
   }
-};
+}
 
-module.exports = {generateToken};
+module.exports = { generateToken };
