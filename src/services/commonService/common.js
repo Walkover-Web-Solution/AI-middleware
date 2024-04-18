8beb0f06b702e0aac5221c39660a8b2b49d93751
Nauman Khan Gori <nauman@whozzat.com>
2024-04-18 16:48:34 +0530

    Bug related to webhook and rtlayer and api key solved

--- src/services/commonService/common.js
+++ src/services/commonService/common.js
@@ -79,6 +79,7 @@ const prochat = async (req, res) => {
     let { apikey, bridge_id, configuration, thread_id, org_id, user, tool_call, service, variables } = req.body;
     let usage = {}, modelResponse = {}, customConfig = {};
     let model = configuration?.model;
+    let rtlLayer=false;
     try {
         const getconfig=await getConfiguration(configuration,service,bridge_id,apikey);
         if(!getconfig.success){
@@ -88,7 +89,7 @@ const prochat = async (req, res) => {
         service = getconfig.service;
         apikey=getconfig.apikey;
         model = configuration?.model;
-        const rtlLayer = getconfig.RTLayer;
+        rtlLayer = getconfig.RTLayer;
         const bridge=getconfig.bridge;
         const apiCallavailable= bridge?.is_api_call ?? false;
         if (!(service in services && services[service]["chat"].has(model))) {
@@ -197,6 +198,14 @@ const geminiResponse = await runChat(geminiConfig,apikey,"chat");
                 if (!geminiResponse?.success) {
                     usage={service:service,model:model,orgId:org_id,latency:Date.now() - startTime,success:false,error:geminiResponse?.error};
                     create([usage])
+                    if(rtlLayer){
+                        rtlayer.message({
+                            ...req.body,
+                            error: geminiResponse?.error,
+                            success: false
+                          }, req.body.rtlOptions);
+                        return 
+                    }
                     return res.status(400).json({ success: false, error: geminiResponse?.error });
                 }
                 usage["totalTokens"] = _.get(geminiResponse, modelOutputConfig.usage[0].total_tokens);
@@ -219,7 +228,6 @@ savehistory(thread_id, user,
             return;
         }
         if(rtlLayer){
-            console.log("ddd",modelResponse)
             rtlayer.message({
                 ...req.body,
                 response: modelResponse,
@@ -238,6 +246,16 @@ savehistory(thread_id, user,
         usage={...usage,service:service,model:model,orgId:org_id,latency:latency,success:false,error:error.message};
         create([usage]);
         console.log("prochats common error=>", error);
+        if(rtlLayer){
+            rtlayer.message({
+                ...req.body,
+                error: error?.message,
+                success: false
+              }, req.body.rtlOptions);
+            return 
+            }
+
+
         return res.status(400).json({ success: false, error: error.message });
     }
 }
@@ -298,6 +316,7 @@ const proCompletion =async (req,res)=>{
     let { apikey,bridge_id,configuration,org_id,prompt,service,variables} = req.body;
     let model = configuration?.model;
     let usage={}, modelResponse = {},customConfig={};
+    let rtlLayer=false;
     try {
         const getconfig=await getConfiguration(configuration,service,bridge_id,apikey);
         if(!getconfig.success){
@@ -307,8 +326,13 @@ const proCompletion =async (req,res)=>{
         service = getconfig.service;
         apikey=getconfig.apikey;
         model = configuration?.model;
+        rtlLayer = getconfig.RTLayer;
         if (!(service in services && services[service]["completion"].has(model))) {
             return res.status(400).json({ success: false, error: "model or service does not exist!" });
+        }
+        const { webhook, headers = {} } = configuration;
+        if(rtlLayer || webhook){
+            res.status(200).json({ success: true,message:"Will got reponse over your configured means." });
         }
         const modelname = model.replaceAll("-", "_").replaceAll(".", "_");
         const modelfunc = ModelsConfig[modelname];
@@ -337,6 +361,14 @@ const proCompletion =async (req,res)=>{
                 if (!openAIResponse?.success) {
                     usage={service:service,model:model,orgId:org_id,latency:Date.now() - startTime,success:false,error:openAIResponse?.error};
                     create([usage])
+                    if(rtlLayer){
+                        rtlayer.message({
+                            ...req.body,
+                            error: openAIResponse?.error,
+                            success: false
+                          }, req.body.rtlOptions);
+                        return 
+                        }
                     return res.status(400).json({ success: false, error: openAIResponse?.error });
                 }
                 usage["totalTokens"] = _.get(modelResponse, modelOutputConfig.usage[0].total_tokens);
@@ -355,6 +387,14 @@ const geminiResponse=await runChat(geminiConfig,apikey,"completion");
                 if (!geminiResponse?.success) {
                     usage={service:service,model:model,orgId:org_id,latency:Date.now() - startTime,success:false,error:geminiResponse?.error};
                     create([usage])
+                    if(rtlLayer){
+                        rtlayer.message({
+                            ...req.body,
+                            error: geminiResponse?.error,
+                            success: false
+                          }, req.body.rtlOptions);
+                        return 
+                    }
                     return res.status(400).json({ success: false, error: geminiResponse?.error });
                 }
                 usage["totalTokens"] = _.get(geminiResponse, modelOutputConfig.usage[0].total_tokens);
@@ -364,16 +404,24 @@ const geminiResponse=await runChat(geminiConfig,apikey,"completion");
                 break;
         }
         const endTime = Date.now();
-        const { webhook, headers = {} } = configuration;
-        if (webhook) {    
-            await sendRequest(webhook, { response: modelResponse, ...req.body }, 'POST', headers);
-            return;
-        }
+        
         const thread_id = uuidv1();
         savehistory(thread_id,prompt,_.get(modelResponse, modelOutputConfig.message),org_id,bridge_id,configuration?.model,'completion',"assistant");
 
         usage={...usage,service:service,model:model,orgId:org_id,latency:endTime - startTime,success:true};
         create([usage])
+        if (webhook) {    
+            await sendRequest(webhook, { response: modelResponse, ...req.body }, 'POST', headers);
+            return;
+        }
+        if(rtlLayer){
+            rtlayer.message({
+                ...req.body,
+                response: modelResponse,
+                success: true
+              }, req.body.rtlOptions);
+            return;
+        }
         return res.status(200).json({ success: true, response: modelResponse });
 
     }
@@ -383,6 +431,14 @@ const geminiResponse=await runChat(geminiConfig,apikey,"completion");
         usage={...usage,service:service,model:model,orgId:org_id,latency:latency,success:false,error:error.message};
         create([usage]);
         console.log("proCompletion common error=>", error);
+        if(rtlLayer){
+            rtlayer.message({
+                ...req.body,
+                error: error?.message,
+                success: false
+              }, req.body.rtlOptions);
+            return 
+            }
         return res.status(400).json({ success: false, error: error.message });
     }
 }
@@ -445,6 +501,7 @@ const proEmbeddings =async (req,res)=>{
     let { apikey,bridge_id,configuration,org_id,input, service } = req.body;
     let model = configuration?.model;
     let usage={}, modelResponse = {},customConfig={};
+    let rtlLayer=false;
     try {
         const getconfig=await getConfiguration(configuration,service,bridge_id,apikey);
         if(!getconfig.success){
@@ -453,9 +510,14 @@ const proEmbeddings =async (req,res)=>{
         configuration=getconfig.configuration;
         service = getconfig.service;
         apikey=getconfig.apikey;
+        rtlLayer=getconfig.RTLayer;
         model = configuration?.model;
         if (!(service in services && services[service]["embedding"].has(model))) {
             return res.status(400).json({ success: false, error: "model or service does not exist!" });
+        }
+        const { webhook, headers = {} } = configuration;
+        if(rtlLayer || webhook){
+            res.status(200).json({ success: true,message:"Will got reponse over your configured means." });
         }
         const modelname = model.replaceAll("-", "_").replaceAll(".", "_");
         const modelfunc = ModelsConfig[modelname];
@@ -473,6 +535,14 @@ const proEmbeddings =async (req,res)=>{
                 if (!response?.success) {
                     usage={service:service,model:model,orgId:org_id,latency:Date.now() - startTime,success:false,error:response?.error};
                     create([usage])
+                    if(rtlLayer){
+                        rtlayer.message({
+                            ...req.body,
+                            error: response?.error,
+                            success: false
+                          }, req.body.rtlOptions);
+                        return 
+                    }
                     return res.status(400).json({ success: false, error: response?.error });
                 }
                 usage["totalTokens"] = _.get(modelResponse, modelOutputConfig.usage[0].total_tokens);
@@ -490,6 +560,14 @@ const proEmbeddings =async (req,res)=>{
                 if (!geminiResponse?.success) {
                     usage={service:service,model:model,orgId:org_id,latency:Date.now() - startTime,success:false,error:geminiResponse?.error};
                     create([usage])
+                    if(rtlLayer){
+                        rtlayer.message({
+                            ...req.body,
+                            error: geminiResponse?.error,
+                            success: false
+                          }, req.body.rtlOptions);
+                        return 
+                    }
                     return res.status(400).json({ success: false, error: geminiResponse?.error });
                 }
                 usage["totalTokens"] = _.get(geminiResponse, modelOutputConfig.usage[0].total_tokens);
@@ -499,16 +577,22 @@ const proEmbeddings =async (req,res)=>{
                 break;
         }
         const endTime = Date.now();
-        const webhook= configuration?.webhook;
-        const headers= configuration?.headers||{}
-        if(webhook){
-            await sendRequest(webhook,{response:modelResponse,...req.body},'POST',headers)
-            return
-        }
         const thread_id = uuidv1();
         savehistory(thread_id,input,JSON.stringify(_.get(modelResponse, modelOutputConfig.message)),org_id,bridge_id,configuration?.model,'embedding',"assistant");
         usage={...usage,service:service,model:model,orgId:org_id,latency:endTime - startTime,success:true};
         create([usage])
+        if(webhook){
+            await sendRequest(webhook,{response:modelResponse,...req.body},'POST',headers)
+            return
+        }
+        if(rtlLayer){
+            rtlayer.message({
+                ...req.body,
+                response: modelResponse,
+                success: false
+              }, req.body.rtlOptions);
+            return 
+        }
         return res.status(200).json({ success: true, response: modelResponse });
     }
     catch(error){
@@ -517,6 +601,14 @@ const proEmbeddings =async (req,res)=>{
         usage={...usage,service:service,model:model,orgId:org_id,latency:latency,success:false,error:error.message};
         create([usage]);
         console.log("proembeddings common error=>", error);
+        if(rtlLayer){
+            rtlayer.message({
+                ...req.body,
+                error: error.message,
+                success: false
+              }, req.body.rtlOptions);
+            return 
+        }
         return res.status(400).json({ success: false, error: error.message });
     }
 }
