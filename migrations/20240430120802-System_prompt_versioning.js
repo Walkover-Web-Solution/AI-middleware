6cff2a11a1f88d9fb6bc76e7087bef975521ee25
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-04-30 18:17:31 +0530

    migration up

---
+++ migrations/20240430120802-System_prompt_versioning.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('System_prompt_versioning', {
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      system_prompt: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      bridge_id: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('System_prompt_versioning');
+  }
+};
+
