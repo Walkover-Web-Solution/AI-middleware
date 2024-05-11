ccdb158e989d9c3f7da9836b13b36293fe2d4a58
Parakh Jain <parakhjain@Parakhs-iMac.local>
2024-05-11 13:59:38 +0530

    chatbot files

---
+++ mongoModel/interfaceModal.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const InterfaceSchema = new Schema({
+  config: {
+    type: Object,
+  },
+  orgId: {
+    type: String,
+  },
+  projectId: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  updatedBy: {
+    type: String,
+  },
+  components: {
+    type: Map,
+    of: Object,
+    default: undefined,
+  },
+  coordinates: {
+    type: Map,
+    of: Object,
+  },
+  accessType: {
+    type: String,
+  },
+
+  actions: [
+    {
+      actionIdMapping: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Actions',
+      },
+      actionId: {
+        type: String,
+      },
+      gridId: {
+        type: String,
+      },
+      componentId: {
+        type: String,
+      },
+    },
+  ],
+  frontendActions: {
+    type: Object,
+  },
+}, { strict: false, minimize: false });
+
+// Create the model from the schema
+const InterfaceModel = mongoose.model('Interfaces', InterfaceSchema);
+export default InterfaceModel;
