fbf2d4c568e955cc199ce2d656727a2845f34fc1
Parakh Jain <parakhjain@Parakhs-iMac.local>
2024-05-14 11:44:54 +0530

    change to common js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const db = require("./models/index.js")
-const modelController=require('./src/controllers/modelController');
-const configurationController=require('./src/controllers/configController');
+const modelController = require('./src/controllers/modelController');
+const configurationController = require('./src/controllers/configController');
 const app = express();
 const PORT = process.env.PORT || 7072;
 const mongoose = require('mongoose');
-const config=require('./config/config.js');
-const  metrisRoutes  =require('./routes/metrics_routes.js');
+const config = require('./config/config.js');
+const metrisRoutes = require('./routes/metrics_routes.js');
 const interfaceUtilties = require('./routes/interface_utility_routes.js');
-const chatbot = require('./routes/chatBot_routes.js') 
+const chatbot = require('./routes/chatBot_routes.js')
 app.use(
   cors({
     origin: '*',
@@ -26,20 +26,19 @@ try {
     useUnifiedTopology: true
   });
 } catch (err) {
-  console.log('database connection error: ',err.message);
+  console.log('database connection error: ', err.message);
   // logger.error('database connection error: ' + err.message);
 }
 app.get('/healthcheck', async (req, res) => {
   res.status(200).send('OK running good...');
 });
-app.use('/api/v1/model',modelController);
-app.use('/api/v1/config',configurationController);
+app.use('/api/v1/model', modelController);
+app.use('/api/v1/config', configurationController);
 // app.use('/interface',interfaces);
-app.use('/chatbot',chatbot);
-app.use('/interfaceUtilites', interfaceUtilties);
+app.use('/chatbot', chatbot);
 
 //Metrics
-app.use('/api/v1/metrics',metrisRoutes)
+app.use('/api/v1/metrics', metrisRoutes)
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  console.log(`Server is running on port ${PORT}`);
+});
