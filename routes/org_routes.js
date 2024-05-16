00a810bc20ba0dcc89f490e9f0241a56c28ea3b2
Harsh Sahu <harshsahu@Pushpendras-Mac-mini.local>
2024-05-16 12:47:27 +0530

    .

--- routes/org_routes.js
+++ routes/org_routes.js
@@ -1,11 +1,8 @@
-const express = require('express');
+import express from "express";
 const router = express.Router();
-const organizationController = require('../src/controllers/orgController');
-
-// Route to create a new organization
+import organizationController from "../src/controllers/orgController"; // Route to create a new organization
 router.post('/organizations', organizationController.createOrganization);
 
 // Route to update response types within an organization
 router.put('/organizations/response-types', organizationController.updateResponseTypes);
-
-module.exports = router;
+export default router;
