7eb45b28280b9e473b1c6a0a86518e795de0ad54
Tanish Jain <walkover@Walkovers-Mac-mini.local>
2024-05-18 11:55:45 +0530

    cherry pick commit 2 from chatbot-in-ai-middleware

--- routes/chatBot_routes.js
+++ routes/chatBot_routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createChatBot, getAllChatBots, getOneChatBot, deleteChatBot, updateChatBot, updateDetails, updateChatBotAction, createAllDefaultResponseInOrg, updateBridge, deleteBridge, addorRemoveResponseIdInBridge, sendMessageUsingChatBot, getChatBotOfBridge } from "../src/controllers/chatBotController.js";
+import { createChatBot, getAllChatBots, getOneChatBot, deleteChatBot, updateChatBot, updateDetails, updateChatBotAction, createAllDefaultResponseInOrg, updateBridge, deleteBridge, addorRemoveResponseIdInBridge, sendMessageUsingChatBot, getChatBotOfBridge, getAllDefaultResponseInOrg, addorRemoveBridgeInChatBot } from "../src/controllers/chatBotController.js";
 const routes = express.Router();
 routes.route('/').post(createChatBot); // create chatbot
 routes.route('/:org_id/all').get(getAllChatBots); // get all chatbot
@@ -7,9 +7,14 @@ routes.route('/:botId').get(getOneChatBot); // get one chatbot
 routes.route('/:botId').put(updateChatBot); // update chatbot
 routes.route('/:botId/updateDetails').put(updateDetails); // update chatbot details
 routes.route('/:botId/updateActions').put(updateChatBotAction); // update chatbot actions
-routes.route('/:orgId/createResponse').post(createAllDefaultResponseInOrg); // TODO --- remove orgid from here and from middlware 
-routes.route('/:orgId/:botId/bridge/:bridgeId').put(updateBridge); // update chatbot actions
-routes.route('/:orgId/:botId/bridge/:bridgeId').delete(deleteBridge); // update chatbot actions
+
+routes.route('/:orgId/createResponse').post(createAllDefaultResponseInOrg) // TODO --- remove orgid from here and from middlware 
+routes.route('/:orgId/getAllResponse').get(getAllDefaultResponseInOrg) // TODO --- remove orgid from here and from middlware; 
+
+// routes.route('/:orgId/:botId/bridge/:bridgeId').put(updateBridge); // update chatbot actions
+// routes.route('/:orgId/:botId/bridge/:bridgeId').delete(deleteBridge); // update chatbot actions
+routes.route('/:orgId/:botId/bridge/:bridgeId').put(addorRemoveBridgeInChatBot); // update chatbot actions
+
 routes.route('/:botId').delete(deleteChatBot); // delete chatbot
 routes.route('/:orgId/addresponseid/bridge/:bridgeId').post(addorRemoveResponseIdInBridge); // done on frontend 
 routes.route('/test').post(sendMessageUsingChatBot);
