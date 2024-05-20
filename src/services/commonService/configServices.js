be1f16bfc50d0ba229b64bc9d7b41773a8b6baa0
Naman Jain <namanjain@Namans-Mac-mini.local>
2024-05-20 13:19:59 +0530

    added slugname and bridgetype at bridge updatee

--- src/services/commonService/configServices.js
+++ src/services/commonService/configServices.js
@@ -194,7 +194,9 @@ const updateBridges = async (req, res) => {
       configuration,
       org_id,
       service,
-      apikey
+      apikey,
+      bridgeType,
+      slugName
     } = req.body;
     try {
       await updateBridgeSchema.validateAsync({
@@ -202,7 +204,9 @@ const updateBridges = async (req, res) => {
         configuration,
         org_id,
         service,
-        apikey
+        apikey,
+        bridgeType,
+        slugName
       });
     } catch (error) {
       return res.status(422).json({
@@ -230,7 +234,7 @@ const updateBridges = async (req, res) => {
     const promptText = _.get(configuration, contentLocation);
     await conversationDbService.storeSystemPrompt(promptText, org_id, bridge_id);
     let prev_configuration = helper.updateConfiguration(bridge.configuration, configuration);
-    const result = await configurationService.updateBridges(bridge_id, prev_configuration, org_id, apikey);
+    const result = await configurationService.updateBridges(bridge_id, prev_configuration, org_id, apikey, bridgeType, slugName);
     filterDataOfBridgeOnTheBaseOfUI(result, bridge_id);
     if (result.success) {
       return res.status(200).json(result);
