efd4f72f48d9019a0c1a2889a1e5efd09d310b20
tanish <tanish@whozzat.com>
2024-05-21 17:17:25 +0530

    remvoe gpt key and added validation in models

--- routes/utlility_routes.js
+++ routes/utlility_routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import {defaulResponseMigration} from "../src/controllers/utilityController"
 const routes = express.Router();
-routes.route('/migration/createdefault-response-inorg').post(); 
+routes.route('/migration/createdefault-response-inorg').post(defaulResponseMigration); 
 export default routes;
