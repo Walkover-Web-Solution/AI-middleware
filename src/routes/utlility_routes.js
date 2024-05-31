53efda64a55744bf30fc129e1fb2d87cab203c24
harsh sahu <sahupavan000@gmail.com>
2024-05-31 14:06:11 +0530

    migration file update

--- src/routes/utlility_routes.js
+++ src/routes/utlility_routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {defaulResponseMigration} from "../src/controllers/utilityController.js"
+import {defaulResponseMigration} from "../controllers/utilityController.js"
 const routes = express.Router();
 routes.route('/migration/createdefault-response-inorg').post(defaulResponseMigration); 
 export default routes;
