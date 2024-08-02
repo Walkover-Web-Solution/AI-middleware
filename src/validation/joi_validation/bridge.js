ebda2b48dc2de4581f025192edea63df880b133f
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-08-02 16:08:39 +0530

    joi validation added

--- src/validation/joi_validation/bridge.js
+++ src/validation/joi_validation/bridge.js
@@ -7,7 +7,7 @@ const updateBridgeSchema = Joi.object({
     model: Joi.string().when(Joi.ref('/service'), {
       is: 'google',
       then: Joi.valid("gemini-pro","gemini-1.5-pro","gemini-1.0-pro-vision","gemini-1.0-pro","gemini-1.5-Flash","embedding-001"),
-      otherwise: Joi.string().valid('gpt-3.5-turbo', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-0125', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-16k-0613', 'gpt-4', 'gpt-4-0613', 'gpt-4-1106-preview', 'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-turbo-2024-04-09', 'gpt-4-turbo', 'gpt-4o', 'text-embedding-3-large', 'text-embedding-3-small', 'text-embedding-ada-002', 'gpt-3.5-turbo-instruct')
+      otherwise: Joi.string().valid('gpt-3.5-turbo', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-0125', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-16k-0613', 'gpt-4', 'gpt-4-0613', 'gpt-4-1106-preview', 'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-turbo-2024-04-09', 'gpt-4-turbo', 'gpt-4o','gpt-4o-mini', 'text-embedding-3-large', 'text-embedding-3-small', 'text-embedding-ada-002', 'gpt-3.5-turbo-instruct')
     }).required(),
     type: Joi.string().valid('chat', 'embedding', 'completion'),
     prompt: Joi.alternatives().try(Joi.string().allow(''), Joi.array()).optional(),
@@ -21,7 +21,7 @@ const updateBridgeSchema = Joi.object({
     'number.base': 'The input must be a number'
   })
 }).when(Joi.object({
-  'configuration.model': Joi.string().valid('gpt-3.5-turbo', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-0125', 'gpt-3.5-turbo-0301', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-16k-0613', 'gpt-4', 'gpt-4-0613', 'gpt-4-1106-preview', 'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-turbo-2024-04-09', 'gpt-4-turbo', 'gpt-4o', 'text-embedding-3-large', 'text-embedding-3-small', 'text-embedding-ada-002', 'gpt-3.5-turbo-instruct', 'gemini-pro','gemini-1.5-pro','gemini-1.0-pro-vision','gemini-1.0-pro','gemini-1.5-Flash')
+  'configuration.model': Joi.string().valid('gpt-3.5-turbo', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-0125', 'gpt-3.5-turbo-0301', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-16k-0613', 'gpt-4', 'gpt-4-0613', 'gpt-4-1106-preview', 'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-turbo-2024-04-09', 'gpt-4-turbo', 'gpt-4o','gpt-4o-mini', 'text-embedding-3-large', 'text-embedding-3-small', 'text-embedding-ada-002', 'gpt-3.5-turbo-instruct', 'gemini-pro','gemini-1.5-pro','gemini-1.0-pro-vision','gemini-1.0-pro','gemini-1.5-Flash')
 }).unknown(), {
   then: Joi.object({
     configuration: Joi.object({
@@ -132,6 +132,19 @@ const updateBridgeSchema = Joi.object({
         "tool_choice": Joi.string()
       }),
       'gpt-4o': Joi.object({
+        "temperature": Joi.string(),
+        "max_tokens": Joi.string(),
+        "top_p": Joi.string(),
+        "logprobs": Joi.string(),
+        "frequency_penalty": Joi.string(),
+        "presence_penalty": Joi.string(),
+        "n": Joi.string(),
+        "stop": Joi.string(),
+        "stream": Joi.string(),
+        "tools": Joi.string(),
+        "tool_choice": Joi.string()
+      }),
+      'gpt-4o-mini': Joi.object({
         "temperature": Joi.string(),
         "max_tokens": Joi.string(),
         "top_p": Joi.string(),
