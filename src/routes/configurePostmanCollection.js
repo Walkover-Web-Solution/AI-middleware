bdc0da95b50edf5ed1da25990dd7c1a99f8c1875
Harsh sahu <rohitmirchandani365@gmail.com>
2024-08-09 13:40:35 +0530

    updteMaxtoken added

--- src/routes/configurePostmanCollection.js
+++ src/routes/configurePostmanCollection.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { importPostmanCollection,configDB } from "../controllers/configurePostmanCollection.js";
+import { importPostmanCollection,updteMaxtoken } from "../controllers/configurePostmanCollection.js";
 import { multerUploads } from '../middleware/multerMiddleware.js';
 const routes = express.Router();
 
 routes.post('/', multerUploads.single('avatar'), importPostmanCollection);
-routes.get('/miragete/configrationDb', configDB);
+routes.get('/miragete/configrationDb', updteMaxtoken);
 
 export default routes;
