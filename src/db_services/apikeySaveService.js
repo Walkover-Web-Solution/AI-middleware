5d8202b6ad67d832b8f37fcf82cbf60a28798cfa
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-08-13 17:41:47 +0530

    apikey orgwise task

---
+++ src/db_services/apikeySaveService.js
@@ -0,0 +1,70 @@
+import apiSaveModel from "../mongoModel/apiModel.js";
+
+const saveApi = async (data) => {
+    try {
+        const { org_id, apikey, service, name } = data;
+        const result = await new apiSaveModel({
+            org_id,
+            apikey,
+            service,
+            name
+        }).save();
+
+        return {
+            success: true,
+            api: result
+        };
+    } catch (error) {
+        console.error("Error saving API: ", error);
+        return {
+            success: false,
+            error: error.message
+        };
+    }
+};
+
+const getName = async (name, org_id)=>{
+    try {
+        const result = await apiSaveModel.findOne({
+        org_id: org_id,
+        name: name
+        })
+
+        return {
+            success: true,
+            result : result
+        }
+    }
+    catch (error) {
+        console.error("Error getting API: ", error);
+        return {
+            success: false,
+            error: error.message
+        };
+    }
+
+}
+const getAllApi = async(org_id)=>{
+    try {
+        const result  = await apiSaveModel.find({
+            org_id: org_id
+        })
+        return {
+            success: true,
+            result : result
+        }
+    } 
+    catch(error) {
+        console.error("Error getting all API: ", error);
+        return {
+            success: false,
+            error: error.message
+        };
+    }
+}
+
+export default {
+    saveApi,
+    getName,
+    getAllApi
+}
---
+++ src/services/commonService/apiKeyService.js
@@ -0,0 +1,62 @@
+import apikeySaveService from "../../db_services/apikeySaveService.js";
+import { services } from "../../configs/models.js";
+
+const saveApikey = async(req,res) => {
+    try {
+        const {apikey, service, org_id, name} = req.body;
+        if (!org_id || !apikey || !service || !name) {
+            return res.status(400).json({
+                success: false,
+                error: "Missing required fields: org_id, apikey, service, and name are all required."
+              });
+        }
+        if (!(service in services)) {
+            return res.status(400).json({
+              success: false,
+              error: "service does not exist!"
+            });
+          }
+        const apiName = await apikeySaveService.getName(name, org_id);
+        if(apiName.success == true && apiName.result != null){
+            return res.status(400).json({
+                success: false,
+                error: "apikey Name already exists!!!"
+              });
+        }
+        const result = await apikeySaveService.saveApi({org_id, apikey, service, name});
+        if(result.success){
+            return res.status(200).json(result);
+        }
+        else {
+            return res.status(400).json({ success: false, error: result.error });
+        }
+    }
+    catch(error){
+        console.error("Error saving API key: ", error);
+        return res.status(400).json({success: false, error: error.message});
+    }
+}
+
+const getAllApikeys = async(req, res) => {
+    try {
+        const org_id = req.body.org_id;
+        const result = await apikeySaveService.getAllApi(org_id);
+        if (result.success) {
+            return res.status(200).json(result);
+        } 
+        else {
+            return res.status(400).json({ success: false, error: result.error });
+        }
+
+    }
+    catch (error) {
+        console.error("Error getting all API keys: ", error);
+        return res.status(400).json({success: false, error: error.message});
+        
+    }
+}
+
+export default{
+  saveApikey,
+  getAllApikeys
+}
