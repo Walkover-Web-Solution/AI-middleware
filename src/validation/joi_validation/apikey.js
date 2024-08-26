670dfcfdcccaf7c141f2e73e80b9ea4f24777c74
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-08-26 17:21:09 +0530

    final changes done

--- src/validation/joi_validation/apikey.js
+++ src/validation/joi_validation/apikey.js
@@ -2,21 +2,22 @@ import Joi from "joi"
 
 const saveApikeySchema = Joi.object({
     name: Joi.string().alphanum().required(),
-    apikey: Joi.string().regex(/^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/).optional().allow(''),
+    apikey: Joi.string().regex(/^[A-Za-z0-9-_]{10,100}$/).required().allow(''),
     service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
-    comment: Joi.string().required()
+    comment: Joi.string().optional()
 })
 
 const updateApikeySchema = Joi.object({
     name: Joi.string().alphanum().optional(),
-    apikey: Joi.string().regex(/^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/).optional().allow(''),
-    apikey_object_id: Joi.string().alphanum().required()
+    apikey: Joi.string().regex(/^[A-Za-z0-9-_]{10,100}$/).optional().allow(''),
+    apikey_object_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+    service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
+    comment : Joi.string().optional()
 })
 
 const deleteApikeySchema = Joi.object({
     apikey_object_id: Joi.string().alphanum().required()
 })
-
 
 
 export {
