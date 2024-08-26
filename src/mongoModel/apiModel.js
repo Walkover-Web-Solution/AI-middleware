4ae2a55e845d90249ddf77fcfeb1b639cf15cbbd
Walk-MAC <walk-mac@Prince.local>
2024-08-26 16:01:15 +0530

    temp

---
+++ src/mongoModel/apiModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+// add schema validation here 
+const ApikeyCredentials = new mongoose.Schema({
+    org_id: {
+        type: String,
+        default: ""
+    },
+    name: {
+        type: String,
+        default: ""
+    },
+    service: {
+        type: String,
+        default: ""
+    },
+    apikey: {
+        type: String,
+        default: ""
+    },
+    comment: {
+        type: String,
+        default: ""
+    } 
+});
+ApikeyCredentials.index({name: 1, org_id: 1}, {unique: true})
+const ApikeyCredential = mongoose.model("ApikeyCredentials", ApikeyCredentials);
+export default ApikeyCredential;
