d30f12b8acfbec544a567ebdbc1b14b5f11c5e08
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-08-27 16:32:13 +0530

    apikey optional

--- src/validation/joi_validation/apikey.js
+++ src/validation/joi_validation/apikey.js
@@ -2,14 +2,14 @@ import Joi from "joi"
 
 const saveApikeySchema = Joi.object({
     name: Joi.string().alphanum().required(),
-    apikey: Joi.string().required().allow(''),
+    apikey: Joi.string().required().optional(),
     service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
     comment: Joi.string().optional()
 })
 
 const updateApikeySchema = Joi.object({
     name: Joi.string().alphanum().optional(),
-    apikey: Joi.string().allow(''),
+    apikey: Joi.string().optional(),
     apikey_object_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
     service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
     comment : Joi.string().optional()
