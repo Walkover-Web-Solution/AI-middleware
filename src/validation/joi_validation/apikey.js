48928382a72be0d738b98d8172faf5349815c627
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-08-27 16:28:40 +0530

    validation for api key removed

--- src/validation/joi_validation/apikey.js
+++ src/validation/joi_validation/apikey.js
@@ -2,14 +2,14 @@ import Joi from "joi"
 
 const saveApikeySchema = Joi.object({
     name: Joi.string().alphanum().required(),
-    apikey: Joi.string().regex(/^[A-Za-z0-9-_]{10,100}$/).required().allow(''),
+    apikey: Joi.string().required().allow(''),
     service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
     comment: Joi.string().optional()
 })
 
 const updateApikeySchema = Joi.object({
     name: Joi.string().alphanum().optional(),
-    apikey: Joi.string().regex(/^[A-Za-z0-9-_]{10,100}$/).optional().allow(''),
+    apikey: Joi.string().allow(''),
     apikey_object_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
     service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
     comment : Joi.string().optional()
