d180e0c53f88d2604de33622db059258458db2fb
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-08-28 15:30:42 +0530

    Api Key org wise (#177)
    * ready to merge in master
    
    * apikey required
    
    ---------
    
    Co-authored-by: Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>

---
+++ src/validation/joi_validation/apikey.js
@@ -0,0 +1,27 @@
+import Joi from "joi"
+
+const saveApikeySchema = Joi.object({
+    name: Joi.string().alphanum().required(),
+    apikey: Joi.string().required().required(),
+    service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
+    comment: Joi.string().optional()
+})
+
+const updateApikeySchema = Joi.object({
+    name: Joi.string().alphanum().optional(),
+    apikey: Joi.string().optional(),
+    apikey_object_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+    service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').optional(),
+    comment : Joi.string().optional()
+})
+
+const deleteApikeySchema = Joi.object({
+    apikey_object_id: Joi.string().alphanum().required()
+})
+
+
+export {
+    saveApikeySchema,
+    updateApikeySchema,
+    deleteApikeySchema
+}
