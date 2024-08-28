a73965c123123aa807404598a2c1eb6870c3e54b
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-08-28 15:30:19 +0530

    apikey required

--- src/validation/joi_validation/apikey.js
+++ src/validation/joi_validation/apikey.js
@@ -2,7 +2,7 @@ import Joi from "joi"
 
 const saveApikeySchema = Joi.object({
     name: Joi.string().alphanum().required(),
-    apikey: Joi.string().required().optional(),
+    apikey: Joi.string().required().required(),
     service : Joi.string().valid('openai', 'google', 'anthropic', 'groq').required(),
     comment: Joi.string().optional()
 })
