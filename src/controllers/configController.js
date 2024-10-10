0ff6f3998648a0553859839906af97ef643a7010
ViaSocket-Git <abbassadri786@gmail.com>
2024-10-10 17:49:57 +0530

    Removed Depricated API's

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -3,23 +3,15 @@ import common from "../services/commonService/configServices.js";
 import middleware from "../middlewares/middleware.js";
 import createApi from "../services/commonService/apiCallService.js";
 import {statusMiddleware}  from '../middlewares/statusMiddleware.js';
-import { chatBotAuth } from "../middlewares/interfaceMiddlewares.js";
 let router = express.Router();
-router.get('/getbridges/all', middleware, common.getAllBridges); //Shifted to Python
-router.get('/getbridges/:bridge_id', middleware, common.getBridges); //Shifted to Python
+
 router.get('/threads/:thread_id/:bridge_id', middleware, common.getThreads,statusMiddleware);
 router.get('/history/:bridge_id', middleware, common.getMessageHistory,statusMiddleware);
-router.get('/models/:service', middleware, common.getAIModels);//Shifted to Python
-router.post('/createbridges', middleware, common.createBridges); //Shifted to Python
-router.post('/updatebridges/:bridge_id', middleware, common.updateBridges); //Shifted to Python
-router.put('/createbridges/:bridge_id', middleware, common.updateBridges); //Shifted to Python
 router.delete('/deletebridges/:bridge_id', middleware, common.deleteBridges,statusMiddleware);
 router.get('/gethistory/:thread_id/:bridge_id', middleware, common.getThreads,statusMiddleware); //Public API for getting history for particular thread
-router.get('/gethistory-chatbot/:thread_id/:bridge_slugName', chatBotAuth, common.getThreads,statusMiddleware);//Route Depricated //Public API for getting history for particular thread
 router.post('/createapi/:bridge_id', middleware, createApi.createsApi); //vaisocket embed create api.
 router.put('/:bridge_id', middleware, common.updateBridgeType);
 router.get('/systemprompt/gethistory/:bridge_id/:timestamp', middleware, common.getSystemPromptHistory,statusMiddleware);
-router.get('/getallsystemprompts/:bridge_id', middleware, common.getAllSystemPromptHistory,statusMiddleware); // Depricated
 router.post('/getFineTuneData/:bridge_id', middleware, common.FineTuneData,statusMiddleware);
 router.put('/gethistory/:bridge_id', middleware, common.updateThreadMessage,statusMiddleware);
 router.put('/status/:status', common.updateMessageStatus,statusMiddleware);
