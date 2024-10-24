046daed5f71b9e34550b89ef392f13a02d476429
parakhjain23 <parakhjain2301@gmail.com>
2024-10-24 17:10:06 +0530

    bridge archive unarchive feature main

--- src/mongoModel/configuration.js
+++ src/mongoModel/configuration.js
@@ -61,6 +61,11 @@ const configuration = new mongoose.Schema({
     type: String,
     required: true
   },
+  status:{
+    type: Number,
+    default: 1,
+    required: true
+  },
   responseIds: {
     type: Array,
     default: []
