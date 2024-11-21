7e78031e0fe446b3c7420815ecef21b3b1d64c07
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2024-11-21 17:14:12 +0530

    migrate db

---
+++ migrations/pg/20241121102948-subthread_id_added_in_conversation_db.cjs
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('conversations', 'sub_thread_id', {
+        type: Sequelize.STRING, // Adjust the type as needed
+        allowNull: true, // Set to false if you want to enforce NOT NULL
+    });
+    
+    // Copy data from thread_id to sub_thread_id
+    await queryInterface.sequelize.query(`
+        UPDATE conversations
+        SET sub_thread_id = thread_id
+    `);
+  },
+
+  async down (queryInterface) {
+    await queryInterface.removeColumn('conversations', 'sub_thread_id');
+
+  }
+};
