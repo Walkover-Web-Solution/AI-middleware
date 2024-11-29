774b86c9eadd536d21aed6eeb0b67a7af42715dc
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-11-29 16:29:35 +0530

    migration_user_history (#259)
    * migration_user_history
    
    * --

---
+++ migrations/pg/20241128124024-version_id_added_in_user_table.cjs
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // Add the 'version_id' column to 'user_bridge_config_history'
+    await queryInterface.addColumn('user_bridge_config_history', 'version_id', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: '',
+    });
+
+    // Copy data from 'bridge_id' to 'version_id' for existing rows
+    await queryInterface.sequelize.query(`
+      UPDATE user_bridge_config_history
+      SET version_id = bridge_id
+    `);
+  },
+
+  async down (queryInterface) {
+    // Remove the 'version_id' column in case of rollback
+    await queryInterface.removeColumn('user_bridge_config_history', 'version_id');
+
+    // Optionally, remove validation from 'bridge_id' if needed
+    await queryInterface.changeColumn('user_bridge_config_history', 'bridge_id', {
+      type: queryInterface.sequelize.Sequelize.STRING,
+      validate: {}
+    });
+  }
+};
