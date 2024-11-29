5f6e908a856e17aa6750e2a23f777f2741502c43
Husain Baghwala <husainbw123@gmail.com>
2024-11-29 12:11:35 +0530

    --

--- migrations/pg/20241128124024-version_id_added_in_user_table.cjs
+++ migrations/pg/20241128124024-version_id_added_in_user_table.cjs
@@ -7,7 +7,26 @@ module.exports = {
     await queryInterface.addColumn('user_bridge_config_history', 'version_id', {
       type: Sequelize.STRING,
       allowNull: false,
-      defaultValue: ''
+      defaultValue: '',
+      validate: {
+        isMongoId(value) {
+          if (!/^[a-f\d]{24}$/i.test(value)) {
+            throw new Error('version_id must be a valid MongoDB ObjectID');
+          }
+        }
+      }
+    });
+
+    // Add validation for 'bridge_id' as well
+    await queryInterface.changeColumn('user_bridge_config_history', 'bridge_id', {
+      type: Sequelize.STRING,
+      validate: {
+        isMongoId(value) {
+          if (!/^[a-f\d]{24}$/i.test(value)) {
+            throw new Error('bridge_id must be a valid MongoDB ObjectID');
+          }
+        }
+      }
     });
 
     // Copy data from 'bridge_id' to 'version_id' for existing rows
@@ -20,5 +39,11 @@ module.exports = {
   async down (queryInterface) {
     // Remove the 'version_id' column in case of rollback
     await queryInterface.removeColumn('user_bridge_config_history', 'version_id');
+
+    // Optionally, remove validation from 'bridge_id' if needed
+    await queryInterface.changeColumn('user_bridge_config_history', 'bridge_id', {
+      type: queryInterface.sequelize.Sequelize.STRING,
+      validate: {}
+    });
   }
 };
