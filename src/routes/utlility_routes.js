07fa609882f3c0eba064b228eed68d4baf073af6
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-12-10 11:24:22 +0530

    extranl api (#261)
    * extranl api
    
    * external api

--- src/routes/utlility_routes.js
+++ src/routes/utlility_routes.js
@@ -1,5 +1,8 @@
 import express from "express";
-import {defaulResponseMigration} from "../controllers/utilityController.js"
+// import {defaulResponseMigration} from "../controllers/utilityController.js"
+import middleware from "../middlewares/middleware.js";
+import extraThreadID from "../services/commonService/configServices.js"
 const routes = express.Router();
-routes.route('/migration/createdefault-response-inorg').post(defaulResponseMigration); 
+// routes.route('/migration/createdefault-response-inorg').post(defaulResponseMigration);
+routes.route('/externalReference').post(middleware, extraThreadID.extraThreadID)
 export default routes;
