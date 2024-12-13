42463c2b6851345bcf042adca8756edd103de05b
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-12-13 16:26:35 +0530

    Image support with migarion (#268)
    * history_for_image
    
    * migration
    
    ---------
    
    Co-authored-by: parakhjain23 <parakhjain2301@gmail.com>

---
+++ migrations/pg/20241211115524-url_and_prompt_coloumn.cjs
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('conversations', 'image_url', {
+        type: Sequelize.TEXT, 
+        allowNull: true, 
+    });
+    await queryInterface.addColumn('conversations', 'revised_prompt', {
+        type: Sequelize.TEXT, 
+        allowNull: true,
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.removeColumn('conversations', 'image_url');
+    await queryInterface.removeColumn('conversations', 'revised_prompt');
+  }
+};
+
