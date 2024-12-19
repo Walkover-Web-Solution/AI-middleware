44754e0a5af8c740cf233ca9caf8d177c032b91b
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2024-12-19 11:01:42 +0530

    review changes

--- src/services/commonService/configServices.js
+++ src/services/commonService/configServices.js
@@ -580,18 +580,13 @@ const updateMessageStatus = async (req, res, next) => {
   }
 }
 const userFeedbackCount = async (req, res, next) =>{
-  try {
-    const bridge_id = req.params.bridge_id;
-    const {startDate, endDate, user_feedback} = req.query;
-    
-    const result = await conversationDbService.userFeedbackCounts({bridge_id,startDate, endDate, user_feedback})
-    res.locals = result;
-    req.statusCode = result?.success ? 200 : 400;
-    return next();
-  } catch (error) {
-    console.log("Error While Finding the response Count of User")
-    throw error
-  }
+  const bridge_id = req.params.bridge_id;
+  const {startDate, endDate, user_feedback} = req.query;
+  
+  const result = await conversationDbService.userFeedbackCounts({bridge_id,startDate, endDate, user_feedback})
+  res.locals = result;
+  req.statusCode = 200 
+  return next();
 }
 
 const bridgeArchive = async (req, res) => {
