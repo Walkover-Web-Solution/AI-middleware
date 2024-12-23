3ef2e9269178cc8a00bda393a387e8619730a741
ViaSocket-Git <abbassadri786@gmail.com>
2024-12-23 16:41:03 +0530

    Removed Deprecated API-main

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'express-async-errors';
 import express from "express";
 import cors from "cors";
 // import multer from 'multer';
-import modelController from "./controllers/modelController.js";
 import configurationController from "./controllers/configController.js";
 import apiKeyrouter from "./routes/apikeyRouter.js";
 import helloRoutes from './routes/helloRoutes.js';
@@ -12,14 +11,13 @@ const PORT = process.env.PORT || 7072;
 import mongoose from "mongoose";
 import config from "../config/config.js";
 // import metrisRoutes from "./routes/metrics_routes.js";
-import utlilityRoutes from "./routes/utlility_routes.js";
 import chatbot from "./routes/chatBot_routes.js";
 import userOrgLocalController from "./routes/userOrgLocal_route.js";
 import notFoundMiddleware from './middlewares/notFound.js';
 import errorHandlerMiddleware from './middlewares/errorHandler.js';
+import responseMiddleware from './middlewares/responseMiddleware.js';
 import configurePostmanCollection from './routes/configurePostmanCollection.js';
 import alerting from './routes/alerting_routes.js';
-import { responseMiddleware } from './middlewares/responseMiddleware.js';
 import('./services/cacheService.js')
 app.use(cors({
   origin: '*',
@@ -40,10 +38,8 @@ try {
 app.get('/healthcheck', async (req, res) => {
   res.status(200).send('OK running good...v1.1');
 });
-app.use('/api/v1/model', modelController);
 app.use('/api/v1/config', configurationController);
 app.use('/apikeys', apiKeyrouter);
-app.use('/utility', utlilityRoutes);
 app.use('/chatbot', chatbot);
 app.use('/user', userOrgLocalController);
 app.use('/config',configurePostmanCollection)
@@ -53,6 +49,7 @@ app.use('/thread', threadRoutes);
 
 //Metrics
 // app.use('/api/v1/metrics', metrisRoutes);
+
 app.use(responseMiddleware); // send response
 app.use(notFoundMiddleware); // added at the last, so that it runs after all routes is being checked
 app.use(errorHandlerMiddleware);
