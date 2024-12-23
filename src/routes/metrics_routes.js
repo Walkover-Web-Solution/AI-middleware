faff81ecc5e9f809ad0505c4e9805a4bac475caa
ViaSocket-Git <abbassadri786@gmail.com>
2024-12-23 16:41:03 +0530

    Removed Deprecated API-main

--- src/routes/metrics_routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { metrics, getRequestData, getRequestDataForPg } from "../controllers/request_controller.js";
-const routes = express.Router();
-routes.route('/request/:id').get(getRequestData);
-routes.route('/request/Pg/:id').get(getRequestDataForPg);
-routes.route('/:org_id').get(metrics);
-export default routes;
