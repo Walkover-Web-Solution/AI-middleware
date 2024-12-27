cbc9f2cb2480cd5a7a360dd3aeac09e8f77f8db6
Husain Baghwala <husainbw123@gmail.com>
2024-12-27 10:37:47 +0530

    env added

--- config/timescale_config.js
+++ config/timescale_config.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
-// const timescaleServiceUrl = process.env.TIMESCALE_SERVICE_URL;
+const timescaleServiceUrl = process.env.TIMESCALE_SERVICE_URL;
 export default {
   development: {
-    url: "postgresql://tsdbadmin:wiu3nrnmhljj26z5@cnd9mf6ofq.x5aoank8hh.tsdb.cloud.timescale.com:35241/tsdb?sslmode=require",
+    url: timescaleServiceUrl,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
@@ -14,11 +14,11 @@ export default {
     }
   },
   test: {
-    url: "postgresql://tsdbadmin:wiu3nrnmhljj26z5@cnd9mf6ofq.x5aoank8hh.tsdb.cloud.timescale.com:35241/tsdb?sslmode=require",
+    url: "",
     dialect: 'postgres'
   },
   production: {
-    url: "postgresql://tsdbadmin:wiu3nrnmhljj26z5@cnd9mf6ofq.x5aoank8hh.tsdb.cloud.timescale.com:35241/tsdb?sslmode=require",
+    url: timescaleServiceUrl,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
