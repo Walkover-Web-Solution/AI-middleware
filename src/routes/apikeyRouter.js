c43e6ba3a389be461422068ee8fa425f2b1c3d24
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2025-01-20 17:46:34 +0530

    combined_middlware_added (#308)

--- src/routes/apikeyRouter.js
+++ src/routes/apikeyRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import service from "../services/commonService/apiKeyService.js";
-import middleware from "../middlewares/middleware.js";
+import {middleware} from "../middlewares/middleware.js";
 
 
 let router = express.Router();
