adc831ad820efe4cfc2eea0cb9393b06d4c225ee
Husain Baghwala <husainbw123@gmail.com>
2025-01-20 16:02:23 +0530

    get_thread using pauthkey

--- src/routes/threadRoutes.js
+++ src/routes/threadRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { chatBotAuth } from '../middlewares/interfaceMiddlewares.js';
 import { createThreadController, getAllThreadsController } from '../controllers/threadController.js';
+import middleware from '../middlewares/middleware.js';
 
 const router = Router();
 // Define routes
 router.post('/', chatBotAuth, createThreadController);
-router.get('/:thread_id', chatBotAuth, getAllThreadsController);
+router.get('/:thread_id', [chatBotAuth, middleware], getAllThreadsController);
 
 export default router;
