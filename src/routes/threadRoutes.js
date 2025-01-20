d0235d6e1f7cb6d7bbe32fae36b7518a4d48b732
Husain Baghwala <husainbw123@gmail.com>
2025-01-20 16:49:03 +0530

    chatbotAuth back

--- src/routes/threadRoutes.js
+++ src/routes/threadRoutes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import { chatBotAuth } from '../middlewares/interfaceMiddlewares.js';
 import { createThreadController, getAllThreadsController } from '../controllers/threadController.js';
-import middleware from '../middlewares/middleware.js';
 
 const router = Router();
 // Define routes
 router.post('/', chatBotAuth, createThreadController);
-router.get('/:thread_id', [chatBotAuth, middleware], getAllThreadsController);
+router.get('/:thread_id', chatBotAuth, getAllThreadsController);
 
 export default router;
