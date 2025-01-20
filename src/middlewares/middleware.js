2550e1b41ba8c5b4674ef8ad7c559bc27346ded0
Husain Baghwala <husainbw123@gmail.com>
2025-01-20 18:38:24 +0530

    authroization added for both

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -58,17 +58,31 @@ const combine_middleware = async (req, res, next) => {
       try {
         const decodedToken = jwt.decode(token);
         if (decodedToken) {
-          let checkToken = jwt.verify(token, process.env.CHATBOTSECRETKEY);
-          if (checkToken) {
-            checkToken.org_id = checkToken.org_id.toString();
-            req.profile = checkToken;
-            req.body.org_id = checkToken?.org_id?.toString();
-            if (!checkToken.user) req.profile.viewOnly = true;
+          // Check for middleware authorization
+          let middlewareToken = jwt.verify(token, process.env.SecretKey);
+          if (middlewareToken) {
+            middlewareToken.org_id = middlewareToken.org_id.toString();
+            req.profile = middlewareToken;
+            req.body.org_id = middlewareToken?.org_id?.toString();
+            if (!middlewareToken.user) req.profile.viewOnly = true;
             return next();
           }
         }
       } catch (e) {
-        console.error("Chatbot token verification failed", e);
+        console.error("Middleware token verification failed", e);
+        // Check for chatbot authorization if middleware verification fails
+        try {
+          let chatbotToken = jwt.verify(token, process.env.CHATBOTSECRETKEY);
+          if (chatbotToken) {
+            chatbotToken.org_id = chatbotToken.org_id.toString();
+            req.profile = chatbotToken;
+            req.body.org_id = chatbotToken?.org_id?.toString();
+            if (!chatbotToken.user) req.profile.viewOnly = true;
+            return next();
+          }
+        } catch (e) {
+          console.error("Chatbot token verification failed", e);
+        }
       }
     }
 
