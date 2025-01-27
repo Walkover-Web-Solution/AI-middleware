7136b25addc08c311b107c582773a27b4aa081c0
Natwar589 <natwaarrathor16082003@gmail.com>
2025-01-27 11:49:03 +0530

    update_user_time_zone_details

--- src/controllers/userOrgLocalController.js
+++ src/controllers/userOrgLocalController.js
@@ -25,7 +25,42 @@ const switchUserOrgLocal = async (req, res) => {
     res.status(200).json({ token });
 }
 
+const updateUserDetails = async (req, res) => {
+    // if (process.env.ENVIROMENT !== 'local') return res.status(404).send();
+
+    const PUBLIC_REFERENCEID = req.headers?.['reference-id']
+    const { company_id, company } = req.body;
+    const updateObject = {
+        company_id: company_id,
+        company: company,
+    };
+    try {
+        const apiUrl = `https://routes.msg91.com/${PUBLIC_REFERENCEID}/updateDetails`;
+        const response = await fetch(apiUrl, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                proxy_auth_token: req.headers['proxy_auth_token'],
+                Authkey: process.env.ADMIN_API_KEY
+            },
+            body: updateObject 
+       });
+
+    if (!response.ok) {
+        throw new Error(`API call failed with status: ${response.status}, error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    res.status(200).json({ message: "User details updated successfully", data });
+    } catch (error) {
+        console.error("Error updating user details:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
 export {
     userOrgLocalToken,
-    switchUserOrgLocal
+    switchUserOrgLocal,
+    updateUserDetails
 }
