0be0aef4c29c988fefee671dfaa5bab49878f784
parakhjain23 <parakhjain2301@gmail.com>
2025-01-29 12:57:55 +0530

    create sub thread from ai- main

--- src/controllers/threadController.js
+++ src/controllers/threadController.js
@@ -1,6 +1,9 @@
 // controllers/threadController.js
 import { createThread, getThreads } from '../services/threadService.js';
+import { ResponseSender } from '../services/utils/customRes.js';
 import { generateIdentifier } from '../services/utils/utilityService.js';
+
+const responseSender = new ResponseSender();
 
 // Create a new thread
 async function createThreadController(req, res, next) {
@@ -24,6 +27,48 @@ async function createThreadController(req, res, next) {
     return next();
 }
 
+async function createSubThreadWithAi(req, res, next) {
+    const { name = "", thread_id, subThreadId, user = "", botId } = req.body;
+    const { org_id, user_id } = req.profile;
+    const sub_thread_id = subThreadId || generateIdentifier()
+    let display_name;
+    if (!thread_id || !org_id || !sub_thread_id) {
+        throw new Error('All fields are required');
+    }
+    if (name === "") {
+        // api call to AI
+        display_name = await createSubThreadNameAI({ user })
+    }
+    if (botId) {
+        const channelId = `${botId}${thread_id.trim() ? thread_id.trim() : user_id}${sub_thread_id.trim() ? sub_thread_id.trim() : user_id}`.replace(" ", "_");
+        responseSender.sendResponse({
+            rtlLayer: true,
+            data: { display_name: display_name, threadId: thread_id, subThreadId: subThreadId },
+            reqBody: {
+                "rtlOptions": {
+                    "channel": channelId,
+                    "ttl": 1,
+                    'apikey': process.env.RTLAYER_AUTH
+                }
+            },
+            headers: {}
+        })
+    }
+
+    const thread = await createThread({
+        display_name: display_name || name || sub_thread_id,
+        thread_id,
+        org_id: org_id.toString(),
+        sub_thread_id
+    });
+    res.locals = {
+        thread,
+        success: true
+    };
+    req.statusCode = 201;
+    return next();
+}
+
 // Get all threads
 async function getAllThreadsController(req, res, next) {
     const { thread_id } = req.params;
@@ -35,8 +80,40 @@ async function getAllThreadsController(req, res, next) {
 }
 
 
+async function createSubThreadNameAI({ user }) {
+    try {
+        const response = await fetch(
+            "https://proxy.viasocket.com/proxy/api/1258584/29gjrmh24/api/v2/model/chat/completion",
+            {
+                method: "POST",
+                headers: {
+                    "pauthkey": "1b13a7a038ce616635899a239771044c",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    user: `Generate thread Name for ${user}`,
+                    bridge_id: "6799c8413166c2fc4886669a",
+                    response_type: "text",
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message || 'Unknown error');
+        }
+        return data.response?.data?.content || "";
+    } catch (err) {
+        console.error("Error calling function=>", err);
+        return null;
+    }
+}
+
+
 
 export {
     createThreadController,
+    createSubThreadWithAi,
     getAllThreadsController
 }
--- src/routes/threadRoutes.js
+++ src/routes/threadRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createThreadController, getAllThreadsController } from '../controllers/threadController.js';
-import {combine_middleware} from '../middlewares/middleware.js';
+import { createSubThreadWithAi, createThreadController, getAllThreadsController } from '../controllers/threadController.js';
+import { combine_middleware } from '../middlewares/middleware.js';
 import { chatBotAuth } from '../middlewares/interfaceMiddlewares.js';
 
 const router = Router();
 // Define routes
 router.post('/', chatBotAuth, createThreadController);
+router.post('/new', chatBotAuth, createSubThreadWithAi);
 router.get('/:thread_id', combine_middleware, getAllThreadsController);
 
 export default router;
