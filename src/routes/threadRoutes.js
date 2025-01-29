9f1a0e30e99aeae91d7d696d48a67faddda922ff
Parakh Jain <97786329+parakhjain23@users.noreply.github.com>
2025-01-29 12:55:05 +0530

    create sub thread from ai2 (#313)

--- src/routes/threadRoutes.js
+++ src/routes/threadRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createThreadController, getAllThreadsController } from '../controllers/threadController.js';
-import { combine_middleware} from '../middlewares/middleware.js';
+import { createSubThreadWithAi, createThreadController, getAllThreadsController } from '../controllers/threadController.js';
+import { combine_middleware } from '../middlewares/middleware.js';
 
 const router = Router();
 // Define routes
 router.post('/', combine_middleware, createThreadController);
+router.post('/new', combine_middleware, createSubThreadWithAi);
 router.get('/:thread_id', combine_middleware, getAllThreadsController);
 
 export default router;
