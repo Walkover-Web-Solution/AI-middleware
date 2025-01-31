bf1e68950ef9e8b9f60c43e7a550de38bc246615
Viasocket Interns <developers@whozzat.com>
2025-01-31 10:14:31 +0530

    Am00018/update user time zone details (#315)
    * update_user_time_zone_details
    
    * update timezone userdetails
    
    * update payload of api
    
    ---------
    
    Co-authored-by: Natwar589 <natwaarrathor16082003@gmail.com>

--- src/controllers/userOrgLocalController.js
+++ src/controllers/userOrgLocalController.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import axios from "axios";
 
 function generateAuthToken(user, org) {
 
@@ -25,7 +26,37 @@ const switchUserOrgLocal = async (req, res) => {
     res.status(200).json({ token });
 }
 
+const updateUserDetails = async (req, res) => {
+    const PUBLIC_REFERENCEID = req.headers?.['reference-id'];
+    const { company_id, company } = req.body;
+
+    if (!company_id || !company) {
+        return res.status(400).json({ message: "company_id and company are required." });
+    }
+
+    const updateObject = {
+        company_id,
+        company : {"meta":company?.meta}
+    };
+
+    try {
+        const apiUrl = `https://routes.msg91.com/api/${PUBLIC_REFERENCEID}/updateDetails`;
+        const response = await axios.put(apiUrl, updateObject, {
+            headers: {
+                Authkey: process.env.ADMIN_API_KEY,
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = response.data;
+        res.status(200).json({ message: "User details updated successfully", data });
+    } catch (error) {
+        console.error("Error updating user details:", error);
+        res.status(404).json({ message: "Something went wrong" });
+    }
+};
+
 export {
     userOrgLocalToken,
-    switchUserOrgLocal
+    switchUserOrgLocal,
+    updateUserDetails
 }
