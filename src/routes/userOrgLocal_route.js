c16c6fbfa9bda67972c05b8fff0e9b45a5c76c02
Natwar589 <natwaarrathor16082003@gmail.com>
2025-02-01 07:58:31 +0530

    update user details timezone

--- src/routes/userOrgLocal_route.js
+++ src/routes/userOrgLocal_route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { userOrgLocalToken, switchUserOrgLocal } from "../controllers/userOrgLocalController.js";
+import { userOrgLocalToken, switchUserOrgLocal, updateUserDetails } from "../controllers/userOrgLocalController.js";
 import {middleware} from "../middlewares/middleware.js";
 const routes = express.Router();
 
 routes.route('/localToken').post(userOrgLocalToken); 
 routes.route('/switchOrg').post(middleware,switchUserOrgLocal); 
+routes.route('/updateDetails').put(middleware,updateUserDetails)
 
 export default routes;
