4f7ac99c8bbe3c018dc597126b87e286526b7b48
Husain Baghwala <husainbw123@gmail.com>
2025-02-07 18:35:34 +0530

    first error save

---
+++ migrations/pg/20250207121832-firstAttemptError.cjs
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('raw_data', 'firstAttemptError', {
+        type: Sequelize.TEXT, 
+        allowNull: true, 
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.removeColumn('raw_data', 'firstAttemptError');
+  }
+};
