34ac05dee5c00e8c124f97350dfcf5512f0ae48e
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2025-02-18 18:38:50 +0530

    _

--- src/db_services/rag_parent_data.js
+++ src/db_services/rag_parent_data.js
@@ -3,14 +3,7 @@ import ragParentDataModel from '../mongoModel/rag_parent_data.js';
 
 
 async function getAll(query){
- try {
-    console.log("Query:", query);
-    const result = await ragParentDataModel.find(query);
-    console.log("Result:", result);
-    return result;
- } catch (error) {
-    return error
- }
+    return await ragParentDataModel.find(query);
  }
 
 export default{
