57afc86582c23a0dcfec717c2ea876cb8575413a
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2025-02-20 18:05:27 +0530

    pinecone curd

--- src/db_services/rag_parent_data.js
+++ src/db_services/rag_parent_data.js
@@ -6,6 +6,21 @@ async function getAll(query){
     return await ragParentDataModel.find(query);
  }
 
+async function create(data) {
+    return await ragParentDataModel.create(data);
+}
+
+async function update(id, data) {
+    return await ragParentDataModel.findByIdAndUpdate(id, data, { new: true });
+}
+
+async function remove(id) {
+    return await ragParentDataModel.findByIdAndDelete(id);
+}
+
 export default{
-    getAll
+    getAll,
+    create,
+    update,
+    remove
 }
