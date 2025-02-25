1d45092b1701bdd3f277227b25256bcbb5d1d304
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2025-02-25 17:11:31 +0530

    fix embed save bug

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -45,7 +45,7 @@ const middleware = async (req, res, next) => {
 
     req.profile.org.id = req.profile.org.id.toString();
     req.body.org_id = req.profile.org.id;
-    req.IsEmbedUser = req.profile.user.meta?.type || false
+    req.IsEmbedUser = req.profile.user.meta?.type || req.profile.extraDetails.tokenType || false
     return next();
   } catch (err) {
     console.error("middleware error =>", err);
@@ -171,7 +171,7 @@ const EmbeddecodeToken = async (req, res, next) => {
 const InternalAuth = async (req, res, next)=>{
   return next()
 }
-const ReturnAuth = async (req, res, next)=>{
+const ReturnAuth = async (req, res)=>{
   return res.status(400).json({ message: 'unauthorized user ', auth: req.get('Authorization')});
 }
 export { middleware, combine_middleware, EmbeddecodeToken, InternalAuth, ReturnAuth };
