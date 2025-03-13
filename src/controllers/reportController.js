eb3759792761cbf9ca0adb7e21b1c8d933636bae
Husain Baghwala <husainbw123@gmail.com>
2025-03-13 12:25:09 +0530

    total active bridges and success count added

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,7 +1,6 @@
-import { getallOrgs } from '../utils/reportUtils.js'
 import get_data_from_pg from '../db_services/reportDbservice.js';
 async function getWeeklyreports(req,res, next) {
-    const org_ids = await getallOrgs();
+    const org_ids = req.body.org_ids
     const data = await get_data_from_pg(org_ids);
     res.locals = { data, success: true };
     req.statusCode = 200;
