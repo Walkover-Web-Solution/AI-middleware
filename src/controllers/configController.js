1b06ef1d805598f725ea38a3f661a6b204c85635
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2025-04-03 17:58:16 +0530

    api added to get user update config history (#367)

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -22,4 +22,5 @@ router.put('/status/:status', chatBotAuth, common.updateMessageStatus);
 router.get('/get-message-history-chatbot/:thread_id/:bridge_slugName', chatBotAuth, common.getThreadMessages)
 router.get('/get-message-history/:thread_id/:bridge_id', middleware, common.getThreadMessages)
 router.put('/bridge-status/:bridge_id', middleware, common.bridgeArchive);
+router.get('/getuserupdates/:version_id', middleware, common.getAllUserUpdates);
 export default router;
