2b78dbac360569990eb9479d2fb0cbfa1afa3cc5
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2025-04-05 17:10:47 +0530

    orm query to direct query

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -13,8 +13,6 @@ router.get('/gethistory/:thread_id/:bridge_id', middleware, common.getThreads); 
 router.get('/gethistory-chatbot/:thread_id/:bridge_slugName', chatBotAuth, common.getThreads);//Route Depricated //Public API for getting history for particular thread
 router.get('/gethistory-chatbot/:thread_id/:bridge_slugName/:message_id', chatBotAuth, common.getMessageByMessageId);//Route Depricated //Public API for getting history for particular thread
 router.delete('/deletebridges/:bridge_id', middleware, common.deleteBridges);
-router.get('/gethistory/:thread_id/:bridge_id', middleware, common.getThreads); //Public API for getting history for particular thread
-router.get('/gethistory-chatbot/:thread_id/:bridge_slugName', chatBotAuth, common.getThreads); //Public API for getting history for particular thread+
 router.get('/systemprompt/gethistory/:bridge_id/:timestamp', middleware, common.getSystemPromptHistory);
 router.post('/getFineTuneData/:bridge_id', middleware, common.FineTuneData);
 router.put('/gethistory/:bridge_id', middleware, common.updateThreadMessage);
