9266afaf5123686c85689693cb9ec440bdc186d9
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2025-04-15 15:17:47 +0530

    rag multiple document compatability with script

--- src/consumers/ragConsumer.js
+++ src/consumers/ragConsumer.js
@@ -9,7 +9,7 @@ import rag_parent_data from '../db_services/rag_parent_data.js';
 import { Doc, MongoStorage, OpenAiEncoder, PineconeStorage } from '../services/document.js';
 import logger from '../logger.js';
 import queue from '../services/queue.js';
-import { getChunkingType } from '../utils/ragUtils.js';
+import { getChunkingType, getScriptId } from '../utils/ragUtils.js';
 import { sendAlert } from '../services/utils/utilityService.js';
 
 
@@ -26,34 +26,27 @@ async function processMsg(message, channel) {
         let pipelineStatus = null;
         
         switch (event) {
+            case 'load_multiple': 
+                const multiLoader = new DocumentLoader();
+                const contentArr = await multiLoader.getContent(data.url);
+                let parentDatas = await rag_parent_data.getDocumentsByQuery({ 'source.scriptId' : getScriptId(data.url) }); // We need to get the documents in the order wer are getting content. FIX THIS WHEN IMPLEMENTING REFRESH DOCUMENT
+                if(parentDatas.length < 2){
+                    const clones = await rag_parent_data.insertMany(
+                      Array(contentArr.length - 1)
+                        .fill()
+                        .map(() => ({ ...parentDatas[0], _id : undefined }))
+                    );
+                    parentDatas = [...parentDatas, ...clones];
+                }
+                for (let parent of parentDatas){
+                    await processContent(contentArr.shift(), parent, parent._id.toString()); 
+                }
+                break;
             case 'load':
                 const loader = new DocumentLoader();
                 const content = await loader.getContent(data.url);
-                const data1 = await rag_parent_data.getDocumentById(data.resourceId);
-                const toUpdate = { content }
-                
-                if (data1.chunking_type === 'auto'){
-                    if(data1.source?.fileFormat !== 'csv'){
-                        toUpdate.chunking_type = await getChunkingType(content);
-                        data1.chunking_type = toUpdate.chunking_type
-                    }
-                    toUpdate.is_chunking_type_auto = true
-                }
-
-                const oldContent = data1.content;
-                // TODO: Can we change the final status as "done"
-                if(oldContent === content || oldContent?.equals(content) )  {
-                    pipelineStatus = "chunked";
-                    break;
-                }
-                await rag_parent_data.update(data.resourceId, toUpdate );
-                const queuePayload =  { resourceId,
-                    content,orgId :data1.org_id, 
-                    userId : data1.user_id,
-                     fileFormat: data1.source.fileFormat,
-                     chunkingType :data1.chunking_type
-                    }
-                await queue.publishToQueue(QUEUE_NAME, { event :"chunk" , data : queuePayload })
+                const ragData = await rag_parent_data.getDocumentById(data.resourceId);
+                await processContent(content, ragData, resourceId);
                 pipelineStatus = "loaded";
                 break;
             case 'delete': {
@@ -118,6 +111,35 @@ async function processMsg(message, channel) {
 
 }
 
+
+async function processContent(content, ragParentData, resourceId){
+    const toUpdate = { content }
+    
+    if (ragParentData.chunking_type === 'auto'){
+        if(ragParentData.source?.fileFormat !== 'csv'){
+            toUpdate.chunking_type = await getChunkingType(content);
+            ragParentData.chunking_type = toUpdate.chunking_type
+        }
+        toUpdate.is_chunking_type_auto = true
+    }
+
+    const oldContent = ragParentData.content;
+    // TODO: Can we change the final status as "done"
+    if(oldContent === content || oldContent?.equals?.(content) )  {
+        return;
+    }
+    await rag_parent_data.update(resourceId, toUpdate );
+    const queuePayload = {
+      resourceId,
+      content,
+      orgId: ragParentData.org_id,
+      userId: ragParentData.user_id,
+      fileFormat: ragParentData.source.fileFormat,
+      chunkingType: ragParentData.chunking_type,
+    };
+    await queue.publishToQueue(QUEUE_NAME, { event :"chunk" , data : queuePayload })
+
+}
 export default {
     queueName: QUEUE_NAME,
     process: processMsg,
