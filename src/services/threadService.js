19d5ee5de73e15b0838d9c2c3d1b83d3156d0696
Harsh <harsh@MacBookAir.localdomain>
2025-05-08 19:14:13 +0530

    fix mongo index issue prod

--- src/services/threadService.js
+++ src/services/threadService.js
@@ -1,8 +1,11 @@
 import Thread from '../mongoModel/threadModel.js';
 
 async function createThread(data) {
-    const thread = new Thread(data);
-    return await thread.save();
+    return await Thread.findOneAndUpdate(
+        { thread_id: data.thread_id, sub_thread_id: data.sub_thread_id },
+        data,
+        { upsert: true, new: true }
+    );
 }
 
 async function getThreads(org_id, thread_id) {
