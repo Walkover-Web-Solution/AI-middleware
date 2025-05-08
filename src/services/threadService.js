97c680bd8fb18b1d1838ce262eb7f9dadcda7ce9
Harsh <harsh@MacBookAir.localdomain>
2025-05-08 19:28:57 +0530

    create thread improved

--- src/services/threadService.js
+++ src/services/threadService.js
@@ -1,11 +1,18 @@
 import Thread from '../mongoModel/threadModel.js';
 
 async function createThread(data) {
-    return await Thread.findOneAndUpdate(
-        { thread_id: data.thread_id, sub_thread_id: data.sub_thread_id },
-        data,
-        { upsert: true, new: true }
-    );
+    const existingThread = await Thread.findOne({ 
+        thread_id: data.thread_id, 
+        sub_thread_id: data.sub_thread_id,
+        org_id: data?.org_id
+    });
+    
+    if (!existingThread) {
+        const thread = new Thread(data);
+        return await thread.save();
+    }
+    
+    return existingThread;
 }
 
 async function getThreads(org_id, thread_id) {
