840d057177c15a20e0dad2ac239e73b212338eda
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2025-05-24 13:43:42 +0530

    all template by visiable key

--- src/db_services/templateDbservice.js
+++ src/db_services/templateDbservice.js
@@ -1,8 +1,6 @@
 import templateModel from '../mongoModel/template.js';
 async function getAll(){
-    return await templateModel.find();
-
-
+    return await templateModel.find({ visible: { $ne: false } });
  }
  export default{
     getAll
