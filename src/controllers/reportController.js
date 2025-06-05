18213c36218e33ff64326b103d82a299719af992
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2025-06-05 18:43:19 +0530

    Daily reports (#373)
    * bridge monthly reports
    
    * changes done
    
    * query added for top 3 bridges
    
    * bridge_name_added
    
    * key name chnaged
    
    * total active bridges and success count added
    
    * get to post
    
    * Update reportDbservice.js
    
    * updated code
    
    * optimized code
    
    * review changes
    
    * daily reports testing
    
    ---------
    
    Co-authored-by: harsh <145131947+harshhsahu@users.noreply.github.com>
    Co-authored-by: Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,4 +1,5 @@
-import get_data_from_pg from '../db_services/reportDbservice.js';
+import {get_data_from_pg, get_data_for_daily_report} from '../db_services/reportDbservice.js';
+
 async function getWeeklyreports(req,res, next) {
     const org_ids = req.body.org_ids
     const data = await get_data_from_pg(org_ids);
@@ -8,7 +9,16 @@ async function getWeeklyreports(req,res, next) {
 
 }
 
+async function getDailyreports(req,res, next) {
+    const org_ids = req.body.org_ids
+    const data = await get_data_for_daily_report(org_ids);
+    res.locals = { data, success: true };
+    req.statusCode = 200;
+    return next();
+}
+
 
 export {
-    getWeeklyreports
+    getWeeklyreports,
+    getDailyreports
 }
