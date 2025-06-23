fa523cb0da38c5bf2a2c872d351443f7187ca769
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2025-06-23 19:42:52 +0530

    reverting changes not working so far

--- src/services/document-loader/html/default.js
+++ src/services/document-loader/html/default.js
@@ -1,31 +1,23 @@
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { HtmlToTextTransformer } from "@langchain/community/document_transformers/html_to_text";
 
 export class WebLoader {
     async getContent(url, options) {
-        // Puppeteer loader initialization with increased timeout and added options
         const loader = new PuppeteerWebBaseLoader(url, {
             launchOptions: {
                 timeout: 0,
                 headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox'],
-                timeout: 60000, // Increase timeout to 60 seconds
-                // Uncomment and specify path if Chromium is not installed properly
-                // executablePath: '/path/to/your/chromium', 
+                args: ['--no-sandbox']
             },
             gotoOptions: {
-                waitUntil: 'domcontentloaded',
-                timeout: 60000, // Ensure that loading the page takes no more than 60 seconds
+                waitUntil: 'load'
             }
         });
-
-        // Load documents from the URL
         const docs = await loader.load();
-        
-        // Transform HTML content to text
         const transformer = new HtmlToTextTransformer();
         const textDoc = await transformer.invoke(docs);
-        
         return textDoc[0]?.pageContent || "";
     }
 }
