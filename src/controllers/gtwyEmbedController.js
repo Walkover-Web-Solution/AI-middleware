79799d7d2c94db729066f2a21c34853cb66c863e
Natwar589 <natwarrathor961@gmail.com>
2025-06-27 18:34:54 +0530

    gtwy embed

---
+++ src/controllers/gtwyEmbedController.js
@@ -0,0 +1,47 @@
+import FolderModel from "../mongoModel/gtwyEmbedModel.js";
+import { createProxyToken, getOrganizationById, updateOrganizationData } from "../services/proxyService.js";
+import { generateIdentifier } from "../services/utils/utilityService.js";
+
+const embedLogin = async (req, res) => {
+    const { name: embeduser_name, email: embeduser_email } = req.Embed;
+      const embedDetails = { user_id: req.Embed.user_id, company_id: req?.Embed?.org_id, company_name: req.Embed.org_name, tokenType: 'embed', embeduser_name, embeduser_email,folder_id : req.Embed.folder_id };
+      const response = {
+        ...req?.Embed,
+        user_id: req.Embed.user_id,
+        token: await createProxyToken(embedDetails),
+      };
+      return res.status(200).json({ data: response, message: 'logged in successfully' });
+}
+
+const createEmbed = async (req, res) => {
+    const name = req.body.name;
+    const org_id =  req.profile.org.id
+    const type = "embed"
+    const folder = await FolderModel.create({ name, org_id, type });
+    res.status(200).json({ data:{...folder.toObject(), folder_id: folder._id} });
+}
+
+const getAllEmbed = async (req, res) => {
+    const org_id =  req.profile.org.id
+    const data = await FolderModel.find({org_id})
+    res.status(200).json({ data: data.map(folder => ({...folder.toObject(), folder_id: folder._id})) });
+}
+
+const genrateToken = async (req, res) => {
+  let gtwyAccessToken;
+  const data = await getOrganizationById(req.profile.org.id)
+  gtwyAccessToken = data?.meta?.gtwyAccessToken;
+  if(!gtwyAccessToken) {
+    gtwyAccessToken = generateIdentifier(20);
+    await updateOrganizationData(req.profile.org.id,  {
+      meta: {
+        ...data?.meta,
+        gtwyAccessToken,  
+      },
+    },
+  );
+  }
+  res.status(200).json({ gtwyAccessToken });
+}
+
+export { embedLogin, createEmbed, getAllEmbed, genrateToken };
