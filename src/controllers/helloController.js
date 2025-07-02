4862a2670ac44c343bce8266ae5267ee7cfcf65e
Viasocket Interns <developers@whozzat.com>
2025-07-02 15:32:39 +0530

    public agents (#405)
    Co-authored-by: Natwar589 <natwarrathor961@gmail.com>

--- src/controllers/helloController.js
+++ src/controllers/helloController.js
@@ -4,12 +4,15 @@ import {
     getChannelList
 } from '../utils/helloUtils.js';
 import ConfigurationServices from '../db_services/ConfigurationServices.js';
-import { createThread } from '../services/threadService.js';
 import { subscribeSchema } from '../validation/joi_validation/bridge.js';
 import ModelsConfig from '../configs/modelConfiguration.js';
 // import helloService from '../db_services/helloService.js';
 export const subscribe = async (req, res, next) => {
     // Validate request body
+    const { ispublic } = req.chatBot;
+    let data = {};
+    let thread_id;
+    if (!ispublic) {
     const { error, value } = subscribeSchema.validate(req.body);
     if (error) {
         return res.status(400).json({
@@ -21,43 +24,46 @@ export const subscribe = async (req, res, next) => {
     const { slugName, versionId } = value;
     let Hello_id = value.helloId
     const { org_id } = req.profile;
-    let data = {};
     if(!Hello_id)  data = (await ConfigurationServices.getBridgeBySlugname(org_id, slugName,versionId));
+    }
+    else {
+        const { slugName :url_slugName } = req.body;
+        data = (await ConfigurationServices.getBridgeByUrlSlugname(url_slugName));
+    }
     const model = data?.modelConfig?.model
     const modelName = Object.keys(ModelsConfig).find(key => ModelsConfig[key]().configuration.model.default === model);
     const vision = modelName ? ModelsConfig[modelName]().configuration.vision : null;
     const services = data?.apikey_object_id ? Object.keys(data?.apikey_object_id) : []
 
     try {
-        if(data?.hello_id?.hello_id ?? false)
-            {
-                const hello_id = data?.hello_id?.hello_id;
-                const [widgetInfo, ChannelList] = await Promise.all([
-                    getWidgetInfo(hello_id),
-                    getChannelList(hello_id, thread_id),
-                ]);
-                const socketJwt = await getSocketJwt(hello_id, ChannelList, false);
+        if (data?.hello_id?.hello_id ?? false) {
+            const hello_id = data?.hello_id?.hello_id;
+            const [widgetInfo, ChannelList] = await Promise.all([
+                getWidgetInfo(hello_id),
+                getChannelList(hello_id, thread_id),
+            ]);
+            const socketJwt = await getSocketJwt(hello_id, ChannelList, false);
 
-                // Check for errors
-                if (widgetInfo.error || socketJwt.error || ChannelList.error) {
-                    // Handle errors accordingly
-                    console.error('Error in one of the promises:', {
-                        widgetInfoError: widgetInfo.error,
-                        socketJwtError: socketJwt.error,
-                        ChannelListError: ChannelList.error,
-                    });
-                    throw new Error('Error in one of the promises');
-                }
-                res.locals = {
-                    widgetInfo: { ...widgetInfo, helloId: hello_id},
-                    Jwt: socketJwt,
-                    ChannelList,
-                    mode : vision ? ['human','vision'] : ['human']
-                }
+            // Check for errors
+            if (widgetInfo.error || socketJwt.error || ChannelList.error) {
+                // Handle errors accordingly
+                console.error('Error in one of the promises:', {
+                    widgetInfoError: widgetInfo.error,
+                    socketJwtError: socketJwt.error,
+                    ChannelListError: ChannelList.error,
+                });
+                throw new Error('Error in one of the promises');
             }
-        else{
             res.locals = {
-                mode : vision ? ['vision'] : [],
+                widgetInfo: { ...widgetInfo, helloId: hello_id },
+                Jwt: socketJwt,
+                ChannelList,
+                mode: vision ? ['human', 'vision'] : ['human']
+            }
+        }
+        else {
+            res.locals = {
+                mode: vision ? ['vision'] : [],
             }
         }
         res.locals['supportedServices'] = services
