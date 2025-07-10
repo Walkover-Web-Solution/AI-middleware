b856d62bcfb24546669445b40387bca9f158abe9
Harsh sahu <chanchalgupta@Harshs-Mac-mini.local>
2025-07-10 13:35:45 +0530

    error f not forund

--- src/db_services/auth_service.js
+++ src/db_services/auth_service.js
@@ -64,6 +64,9 @@ const verify_auth_token = async (client_id, redirection_url) => {
 const find_auth_by_client_id = async (client_id) => {
     try {
         const result = await Auth.findOne({ client_id },{name: 1, client_id: 1});
+        if (!result) {
+            throw new Error(`Auth with client_id ${client_id} not found`);
+        }
         return result;
     } catch (error) {
         throw new Error(`Failed to find auth by client_id: ${error.message}`);
