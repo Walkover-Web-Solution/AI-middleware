678e938e05375c977ed560f7ef5055120ad929db
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2025-07-15 14:42:25 +0530

    throwing error when invalid response from flow, rag auth

--- src/services/document-loader/script.js
+++ src/services/document-loader/script.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 export class ScriptLoader {
     async getContent(url, options) {
         const response = await axios.get(url, options);
+        if(!response.data || typeof response.data !== 'object') {
+            throw new Error('Invalid response format');
+        }
         return options?.fileId ? response.data[options.fileId] : response.data;
     }
 }
