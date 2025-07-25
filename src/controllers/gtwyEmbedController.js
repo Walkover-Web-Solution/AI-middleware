5fc6e73b4482bb09590e7c2c2f2c70dd681596da
Natwar589 <natwarrathor961@gmail.com>
2025-07-25 16:17:58 +0530

    add config

--- src/controllers/gtwyEmbedController.js
+++ src/controllers/gtwyEmbedController.js
@@ -18,9 +18,10 @@ const embedLogin = async (req, res) => {
 
 const createEmbed = async (req, res) => {
     const name = req.body.name;
+    const config = req.body.config;
     const org_id =  req.profile.org.id
     const type = "embed"
-    const folder = await FolderModel.create({ name, org_id, type });
+    const folder = await FolderModel.create({ name, org_id, type, config });
     res.status(200).json({ data:{...folder.toObject(), folder_id: folder._id} });
 }
 
