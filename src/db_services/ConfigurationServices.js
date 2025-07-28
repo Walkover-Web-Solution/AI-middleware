69411d893b38ec491659668425e22ae9d11a0d6c
Viasocket Interns <developers@whozzat.com>
2025-07-28 13:40:44 +0530

    file support (#454)
    Co-authored-by: Natwar589 <natwarrathor961@gmail.com>

--- src/db_services/ConfigurationServices.js
+++ src/db_services/ConfigurationServices.js
@@ -178,11 +178,12 @@ const getBridgeBySlugname = async (orgId, slugName, versionId) => {
     }).select({ 'configuration.model': 1, service: 1 , apikey_object_id: 1 }).lean();
 
     const model = versionId ?  modelConfig.configuration  : hello_id?.configuration 
+    const service = versionId ?  modelConfig.service  : hello_id?.service 
     const apikey_object_id = versionId ?  modelConfig.apikey_object_id  : hello_id?.apikey_object_id 
 
     if (!hello_id) return false; 
 
-    return {hello_id ,modelConfig:model, apikey_object_id, service: hello_id.service};
+    return {hello_id ,modelConfig:model, apikey_object_id, service};
   } catch (error) {
     console.log("error:", error);
     return {
