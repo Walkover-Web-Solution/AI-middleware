76bcce269c6dfedee3c38b68c1548752484d0b0a
Viasocket Interns <developers@whozzat.com>
2025-09-27 12:22:54 +0530

    alerts on thumbs down (#533)
    Co-authored-by: harsh mathankar <mathankarharsh31@gmail.com>

---
+++ src/db_services/webhook_alert_dbservice.js
@@ -0,0 +1,18 @@
+import { alerts_Model } from '../mongoModel/alertsModel.js';
+
+async function get_webhook_data(org_id) {
+    try {
+        const webhook_data = await alerts_Model.find({
+            'org_id': org_id
+        });
+        return {
+            'webhook_data': webhook_data || []
+        }
+    } catch (error) {
+        throw new Error(`Error in get_webhook_data: ${error.message}`);
+    }
+}
+
+export {
+    get_webhook_data
+}
