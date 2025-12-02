9f504be83400d3633913eb9d892ea443a25eac51
ViaSocket-Git <developers@whozzat.com>
2025-12-02 18:31:39 +0530

    reverts templates routes

---
+++ src/routes/template_route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { allTemplates } from "../controllers/templateController.js";
+import { middleware } from "../middlewares/middleware.js";
+
+const router = express.Router();
+
+router.get("/all", allTemplates);
+router.get("/token", middleware, (req, res) => {
+  res.json({
+    success: true,
+    data: req.profile,
+  });
+});
+
+export default router;
