c9de7b6508bdc0aad3572ad9ed07079dac74d8dd
Harsh Sahu <harksahu@Harshs-Mac-mini.localdomain>
2025-12-04 19:58:24 +0530

    sync with testing and prod and embed route

---
+++ src/routes/embed.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createEmbed, embedLogin, genrateToken, getAllEmbed, getEmbedDataByUserId, updateEmbed } from "../controllers/embed.controller.js";
+import {middleware } from "../middlewares/middleware.js";
+import { GtwyEmbeddecodeToken } from "../middlewares/gtwyEmbedMiddleware.js";
+const routes = express.Router();
+
+routes.route('/login').get(GtwyEmbeddecodeToken, embedLogin)
+routes.route('/').post(middleware, createEmbed)
+routes.route('/').get(middleware, getAllEmbed)
+routes.route('/').put(middleware, updateEmbed)
+routes.route('/token').get(middleware, genrateToken)
+routes.route('/getAgents').get(GtwyEmbeddecodeToken, getEmbedDataByUserId)
+
+export default routes;
