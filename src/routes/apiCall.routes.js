2b5f823cf6686fd6564f133ae5bbd3d540cb500a
Husain Baghwala <husainhackerrank@gmail.com>
2025-12-23 15:33:02 +0530

    refactor: Enhance route security by requiring admin role or agent access for critical operations
    - Updated agentVersion.routes.js to require admin role for creating, publishing, deleting, and discarding versions.
    - Modified apiCall.routes.js to enforce agent access for updating, deleting, and creating API calls.
    - Adjusted apikey.routes.js to require agent access for managing API keys.
    - Enhanced rag.routes.js to enforce agent access for document deletion and updates.

--- src/routes/apiCall.routes.js
+++ src/routes/apiCall.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { middleware } from "../middlewares/middleware.js";
+import { middleware ,checkAgentAccessMiddleware} from "../middlewares/middleware.js";
 import controller from "../controllers/apiCall.controller.js";
 import validate from '../middlewares/validate.middleware.js';
 import apiCallValidation from '../validation/joi_validation/apiCall.validation.js';
@@ -7,10 +7,10 @@ import apiCallValidation from '../validation/joi_validation/apiCall.validation.j
 const router = express.Router();
 
 router.get('/', middleware, controller.getAllApiCalls);
-router.put('/:function_id', middleware, validate(apiCallValidation.updateApiCalls), controller.updateApiCalls);
-router.delete('/', middleware, validate(apiCallValidation.deleteFunction), controller.deleteFunction);
-router.post('/', middleware, validate(apiCallValidation.createApi), controller.createApi);
-router.put('/pre_tool/:agent_id', middleware, validate(apiCallValidation.addPreTool), controller.addPreTool);
+router.put('/:function_id', middleware, checkAgentAccessMiddleware, validate(apiCallValidation.updateApiCalls), controller.updateApiCalls);
+router.delete('/', middleware, checkAgentAccessMiddleware, validate(apiCallValidation.deleteFunction), controller.deleteFunction);
+router.post('/', middleware, checkAgentAccessMiddleware, validate(apiCallValidation.createApi), controller.createApi);
+router.put('/pre_tool/:agent_id', middleware, checkAgentAccessMiddleware, validate(apiCallValidation.addPreTool), controller.addPreTool);
 router.get('/inbuilt', middleware, controller.getAllInBuiltToolsController);
 
 export default router;
