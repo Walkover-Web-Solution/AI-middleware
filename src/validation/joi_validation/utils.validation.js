a99c6aba2d4ac4bb1e594c102b12bbd48a8f7bf5
Yogesh Patel <yogeshpatel@gmail.com>
2026-02-26 19:24:42 +0530

    fix: embed preview

--- src/validation/joi_validation/utils.validation.js
+++ src/validation/joi_validation/utils.validation.js
@@ -91,7 +91,7 @@ const callAi = {
 const generateToken = {
   body: Joi.object()
     .keys({
-      type: Joi.string().valid("rag", "org", "embed").required()
+      type: Joi.string().valid("rag", "org", "embed", "embed_preview", "rag_embed_preview", "chatbot_embed_preview").required()
     })
     .unknown(true)
 };
